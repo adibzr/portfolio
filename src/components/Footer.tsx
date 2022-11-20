6a3d7cbd006a4c6ab4693cc90e5e6014f34366b0
adibz <adibraed1@gmail.com>
2022-11-20 10:48:33 -0300

    footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="py-5 text-center">
       <p className="text-sm mt-2  opacity-50">
-        &copy; {new Date().getFullYear()} Forrest Knight. All rights reserved.
+        &copy; {new Date().getFullYear()} Adib Raed. All rights reserved.
       </p>
     </div>
   );
