ae22cced01304e47feb5772d4d52efba350d581a
adib <adibraed1@gmail.com>
2022-11-27 17:31:48 -0300

    fixed links

--- src/assets/projects/projects.ts
+++ src/assets/projects/projects.ts
@@ -20,8 +20,8 @@ export const projectsEs: info[] = [
     collaborators: "Solo project",
     description:
       "App de videojuegos que incluye filtrados, ordenamiento, creación  y búsqueda. Usa como fuente la API de Rawg.io.",
-    repoFront: "https://github.com/adibzr/barbershop-front",
-    repoBack: "https://github.com/adibzr/barbershop-back",
+    repoFront: "https://github.com/adibzr/videogames-front",
+    repoBack: "https://github.com/adibzr/videogames-back",
     site: "https://videogames-adib-raed.up.railway.app/home",
   },
   {
@@ -32,8 +32,8 @@ export const projectsEs: info[] = [
     stack: ["React", "Redux-toolkit", "Node/Express", "MongoDB"],
     description:
       "E-commerce que incluye pedido y gestión de turnos, filtrado y búsqueda de productos,  carrito de compra con pasarela de pago, envío de mail, administración de productos, usuarios y compra,",
-    repoFront: "https://github.com/adibzr/videogames-front",
-    repoBack: "https://github.com/adibzr/videogames-back",
+    repoFront: "https://github.com/adibzr/barbershop-front",
+    repoBack: "https://github.com/adibzr/barbershop-back",
     site: "https://barbershop-front-deploy.vercel.app/",
   },
 ];
@@ -46,8 +46,8 @@ export const projectsEn = [
     collaborators: "Solo project",
     description:
       "Videogames App that shows videogames from Rawg.io API. Able to filter, order, create and seach games.",
-    repoFront: "https://github.com/adibzr/barbershop-front",
-    repoBack: "https://github.com/adibzr/barbershop-back",
+    repoFront: "https://github.com/adibzr/videogames-front",
+    repoBack: "https://github.com/adibzr/videogames-back",
     site: "https://videogames-adib-raed.up.railway.app/home",
   },
   {
@@ -58,8 +58,8 @@ export const projectsEn = [
       "Agustin Puggioni, Manuel Seisdedos, Gonzalo Dos Santos, Ignacio Silva, Jaime Gutièrrez",
     description:
       "E-commerce that features apointment creation and management, product filtering and search, product cart and payment system, mailing upon compleate/cancel/create/dispatch product, admin features: product modification, creation, deletion; user ban, convert to admin, purchase history; purchase histories, option to complete/cancel/process/dispatch product",
-    repoFront: "https://github.com/adibzr/videogames-front",
-    repoBack: "https://github.com/adibzr/videogames-back",
+    repoFront: "https://github.com/adibzr/barbershop-front",
+    repoBack: "https://github.com/adibzr/barbershop-back",
     site: "https://barbershop-front-deploy.vercel.app/",
   },
 ];
