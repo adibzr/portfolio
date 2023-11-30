6c5686b25f5fb27f8b50a86c8de75c1b537d864c
Adib Raed <adibraed1@gmail.com>
2023-11-30 00:09:28 +0000

    added animation to about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,5 @@
 import { useAppSelector } from "../hooks";
+import { motion } from "framer-motion";
 
 const About = () => {
   const spanish = useAppSelector((state) => state.lang.spanish);
@@ -6,17 +7,22 @@ const About = () => {
   return (
     <div>
       {spanish ? (
-        <div
+        <motion.div
+        variants={{
+          hidden: {opacity:0, y:-200},
+          fadeIn: {opacity:1, y:0}
+        }}
+        initial="hidden"
+        whileInView="fadeIn"
+        transition={{duration:0.5}}
           id="about"
-          className="h-screen bg-green-500 relative  px-1 overflow-hidden text-[#D7FADB]
-  after:w-full after:h-1/6 after:absolute after:left-0 after:bottom-0 md:after:border-r-[100vw] after:border-white md:after:border-t-[15vh] after:border-t-green-500
-  before:w-full before:h-1/6 before:absolute before:left-0 before:top-0 md:before:border-r-[100vw] before:border-white md:before:border-t-[15vh] before:border-r-green-500
-  "
+          className="h-screen bg-green-500 relative  px-1 overflow-hidden text-[#6c6c6c]
+          lg:bg-background bg-cover bg-center"
         >
           <h2 className="justify-start flex flex-col items-center  pb-8 pt-[8%] font-bold text-4xl ">
             ACERCA DE MI
           </h2>
-          <div className=" md:grid md:m-2 m-4 text-sm grid-cols-[.5fr_1fr] h-full justify-items-center ">
+          <div className=" m-4 text-sm h-full justify-items-center ">
               <div>
                 <p className="my-6">
                   Comencé mis estudios de ingeniería de software en la
@@ -37,15 +43,22 @@ const About = () => {
                 <a
                   href="https://drive.google.com/file/d/1oe5sDaUGdoDZQc1DHEK5fBW9Ef5_fI-_/view?usp=sharing"
                   target={"_blank"}
-                  className=" md:inline-block md:text-[inherit] text-green-500 bg-white md:bg-transparent flex justify-center font-bold px-12 cursor-pointer p-2 border-2 border-white-500 shadow-[inset_0_0_0_0_rgb(34,197,94)] ease-in duration-300 hover:shadow-[inset_400px_0_0_0_rgb(255,255,255)] hover:text-green-500 "
+                  className=" md:inline-block md:text-[inherit] text-green-500 bg-white md:bg-transparent flex justify-center font-bold px-12 cursor-pointer p-2 border-2 border-white-500 hover:shadow-[inset_400px_0_0_0_rgb(255,255,255)] hover:text-green-500 "
                 >
                   CV
                 </a>
               </div>
           </div>
-        </div>
+        </motion.div>
       ) : (
-        <div
+        <motion.div
+        variants={{
+          hidden: {opacity:0, y:-200},
+          fadeIn: {opacity:1, y:0}
+        }}
+        initial="hidden"
+        whileInView="fadeIn"
+        transition={{duration:0.5}}
           id="about"
           className="h-screen bg-green-500 relative  px-1 overflow-hidden text-[#6c6c6c]
           lg:bg-background bg-cover bg-center
@@ -54,7 +67,7 @@ const About = () => {
           <h2 className="justify-start flex flex-col items-center  pb-8 pt-[8%] font-bold text-4xl ">
             ABOUT
           </h2>
-          <div className=" md:grid md:m-2 m-4 text-sm grid-cols-[.5fr_1fr] h-full justify-items-center ">
+          <div className=" m-4 text-sm h-full justify-items-center ">
               <div>
                 <p className="my-6">
                   I began studies in software engineering in Universidad de
@@ -79,13 +92,13 @@ const About = () => {
                     "https://drive.google.com/file/d/1fkuGGAtaz5i0fO6kokma0_AyOUv1Gcq-/view?usp=sharing"
                   }
                   target={"_blank"}
-                  className="md:m-8 m-4 md:inline-block md:text-[inherit] text-green-500 bg-white md:bg-transparent flex justify-center font-bold px-12 cursor-pointer p-2 border-2 border-white-500 shadow-[inset_0_0_0_0_rgb(34,197,94)] ease-in duration-300 hover:shadow-[inset_400px_0_0_0_rgb(255,255,255)] hover:text-green-500 "
+                  className="md:inline-block md:text-[inherit] text-green-500 bg-white md:bg-transparent flex justify-center font-bold px-12 cursor-pointer p-2 border-2 border-white-500 hover:shadow-[inset_400px_0_0_0_rgb(255,255,255)] hover:text-green-500"
                 >
                   CV
                 </a>
               </div>
           </div>
-        </div>
+        </motion.div>
       )}
     </div>
   );
